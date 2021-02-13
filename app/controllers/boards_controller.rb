59fc97f5a349ec2a5418a54b5a0f869f0e35e89f
Daniel Sasse <dsasse07@gmail.com>
2021-02-13 16:38:14 -0500

    Board#show endpoint complete

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,8 @@
+class BoardsController < ApplicationController
+
+  def show
+    board = Board.find(params[:id])
+    render json: board
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'board/show'
   get 'users/', to: 'users#index'
   get 'users/:id', to: 'users#show'
+
+  get 'boards/:id', to: 'boards#show'
 
 end
