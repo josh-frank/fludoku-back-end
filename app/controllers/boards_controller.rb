74b6ac9807b6005cbeb2ecfaca7e890bfdca8996
Josh Frank <eurosewusa@gmail.com>
2021-02-15 14:52:17 -0500

    testing a patch path for boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,6 +5,12 @@ class BoardsController < ApplicationController
     render json: new_board
   end
 
+  def update
+    updated_board = Board.find( params[ :id ] )
+    updated_board.update( board_params )
+    render json: updated_board
+  end
+
   def show
     board = Board.find(params[:id])
     render json: board
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
   get 'board/show'
-  get 'users/', to: 'users#index'
+  get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
 
-  post 'boards/', to: 'boards#create'
+  post 'boards', to: 'boards#create'
+  patch 'boards/:id', to 'boards#update'
   get 'boards/:id', to: 'boards#show'
 
 end
